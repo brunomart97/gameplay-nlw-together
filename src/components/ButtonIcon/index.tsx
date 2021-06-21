b9fc8a73ce14169ccac90bc631b7398718201a19
Bruno Martins <brunomart97@hotmail.com>
2021-06-20 21:54:58 -0300

    SignIn screen

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+import { styles } from './styles';
+import DiscordImg from '../../assets/discord.png';
+
+interface ButtonIconProps {
+  title: string;
+}
+
+export function ButtonIcon({ title } : ButtonIconProps) {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.7} >
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
+
+      <Text style={styles.title}>
+        { title }
+      </Text>
+    </TouchableOpacity>
+  );
+}
