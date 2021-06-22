9b487e584607afc1615f8941c5f5de46a4d8d64e
Bruno Martins <brunomart97@hotmail.com>
2021-06-21 21:26:36 -0300

    Custom fonts

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import {
   View,
   Text,
-  Image,
-  StatusBar
+  Image
 } from 'react-native';
 
 import { styles } from './styles';
@@ -14,12 +13,6 @@ import { ButtonIcon } from '../../components/ButtonIcon';
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-
       <Image
         source={IllustrationImg}
         style={styles.image}
@@ -28,13 +21,13 @@ export function SignIn() {
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </Text>
 
